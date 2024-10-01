e5ce76ccd618f0eece6f8226a0c31edca5197b33
Kai Yee <kaicyee@verizon.net>
2024-10-01 12:07:43 -0400

    Updated emoji-generator.tsx to display replicate emoji properly

--- emoji-maker/app/api/generate-emoji/route.ts
+++ emoji-maker/app/api/generate-emoji/route.ts
@@ -13,8 +13,19 @@ export async function POST(request: Request) {
       "fofr/sdxl-emoji:dee76b5afde21b0f01ed7925f0665b7e879c50ee718c5f78a9d38e04d523cc5e",
       {
         input: {
-          prompt,
-          apply_watermark: false
+          width: 1024,
+          height: 1024,
+          prompt: "A TOK emoji of " + prompt,
+          refine: "no_refiner",
+          scheduler: "K_EULER",
+          lora_scale: 0.6,
+          num_outputs: 1,
+          guidance_scale: 7.5,
+          apply_watermark: false,
+          high_noise_frac: 0.8,
+          negative_prompt: "",
+          prompt_strength: 0.8,
+          num_inference_steps: 50,
         }
       }
     );
